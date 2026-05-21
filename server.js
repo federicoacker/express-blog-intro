388a6a57bd253cb8941d8c55b8fff91c9bee129e
federicoacker <acker.federico@gmail.com>
2026-05-21 14:16:26 +0200

    Cominciato a fare la route per bacheca. Inserito middleware per gli static files per le immagini

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ import express from "express";
 const app = express();
 const SERVER_URL = process.env.SERVER_URL;
 const SERVER_PORT = process.env.SERVER_PORT;
+const PROTOCOL = "http://";
+app.use(express.static("public/imgs"));
 
-
-app.get("/", (request, response) =>{
+app.get("/", (request, response) => {
     response.send("<h1>Server del mio blog</h1>"); //Semplice response in html che è il type di default
+})
+
+app.get("/bacheca", (request, response) => {
+
 })
 
 app.listen(SERVER_PORT, (error) => {
